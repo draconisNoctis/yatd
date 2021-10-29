a12c09a70d6ad7310206fa0c1740859edec843d0
Mark Wecke <wecke@neoskop.de>
2021-10-29 16:49:37 +0200

    feat: add @angular/material

--- projects/model-viewer/src/app/model-viewer.module.ts
+++ projects/model-viewer/src/app/model-viewer.module.ts
@@ -1,6 +1,7 @@
 import { YatdObjectsModule } from '@yatd/objects';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EngineModule } from '@yatd/engine';
 import { YatdUiModule } from '@yatd/ui';
 
@@ -24,7 +25,8 @@ import { UiEffects } from './effects/ui.effects';
         StoreModule.forRoot(reducers, { metaReducers }),
         !environment.production ? StoreDevtoolsModule.instrument() : [],
         EffectsModule.forRoot([UiEffects]),
-        YatdObjectsModule
+        YatdObjectsModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [ModelViewerComponent]
--- projects/mvp/src/app/mvp.module.ts
+++ projects/mvp/src/app/mvp.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MvpComponent } from './mvp.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [MvpComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [MvpComponent],
 })
